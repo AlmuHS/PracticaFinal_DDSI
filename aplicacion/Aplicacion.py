23dbb88d02cf661734c6da0c98bdd8f8a199b4cd
AlmuHS <liberamenso10000@gmail.com>
2017-04-21 20:41:20 +0200

    Updated license

--- aplicacion/Aplicacion.py
+++ aplicacion/Aplicacion.py
@@ -1,3 +1,16 @@
+'''Copyright 2016 2017 Almudena García Jurado-Centurión
+This file is part of PracticaFinal_DDSI
+PracticaFinal_DDSI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+Practica2_AMC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with PracticaFinal_DDSI.  If not, see <http://www.gnu.org/licenses/>'''
+
 import ManejaVoluntario
 import ManejaAlimento
 import Conexion
